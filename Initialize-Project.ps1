0e19f0ed38bd2a9f07ad5fa20d26e4bc51280157
Kirsten Kluge <live@kirkone.de>
2017-12-14 23:09:01 +0100

    added Initialize-Project script

---
+++ Initialize-Project.ps1
@@ -0,0 +1,35 @@
+[CmdletBinding()]
+Param (
+    [Parameter(Mandatory = $True, Position = 1)]    
+    [string]
+    $Project
+)
+
+if (Test-Path -Path ".\Projects\$Project" ) {
+    Write-Error "Project with the name '$Project allready exists!'"
+    return
+}
+
+New-Item -ItemType directory -Path ".\Projects\$Project" | Out-Null
+New-Item -ItemType File -Path ".\Projects\$Project\repositoties.txt" | Out-Null
+
+"[display]
+frameless=false
+multi-sampling=true
+no-vsync=true
+viewport=1280x720
+
+[gource]
+auto-skip-seconds=1
+colour-images=true
+date-format=%d %B %Y
+file-idle-time=0
+filename-time=2
+font-size=36
+hide=
+key=true
+max-file-lag=3
+max-files=0
+max-user-speed=100
+seconds-per-day=10
+title=$Project" | Out-File -FilePath ".\Projects\$Project\config.txt" -NoNewline
